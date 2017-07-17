9c9c1dd1b6a205ff42ffb7a65ce5eae207544f76
Nigel Charman <nigel.charman.nz@gmail.com>
2017-07-17 14:03:39 +1200

    Initial commit

---
+++ web/js/cricketCalc.js
@@ -0,0 +1,27 @@
+var scorecard="";
+var score=0
+
+/**
+    @param {String} key
+    Appends the key to the scorecard, and adds it to the total score.
+    The key must be numeric or a '.'.
+*/
+function enter(key) { 
+    scorecard = scorecard + key;
+    document.getElementById('scorecard').innerHTML = scorecard
+    if (key != '.') score = score + key;
+    document.getElementById('score').innerHTML = score;
+    
+}
+
+/**
+    Removes the last entry from  the scorecard, and subtracts it from the total score.
+*/
+function backspace() {
+    var previousBall = scorecard.charAt(scorecard.length - 1);
+    if(previousBall == ".") previousBall = 0
+    score = score - previousBall;
+    scorecard = scorecard.substring(0, scorecard.length-1);
+    document.getElementById('scorecard').innerHTML = scorecard;
+    document.getElementById('score').innerHTML = score;
+}
